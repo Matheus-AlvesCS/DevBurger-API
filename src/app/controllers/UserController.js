eea7ceea8a74333a2a58185ca47cfbdde7aaa5ae
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2025-03-12 19:30:31 -0300

    Validação de dados e criptografia de senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,16 +1,40 @@
 import { v4 } from "uuid";
+import * as yup from "yup";
 
 import User from "../models/User";
 
 class UserController {
   async store(request, response) {
-    const { name, email, password_hash, admin } = request.body;
+    const userSchema = yup.object({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup.string().min(6).required(),
+      admin: yup.boolean(),
+    });
+
+    try {
+      userSchema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { name, email, password, admin } = request.body;
+
+    const alreadyExists = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (alreadyExists) {
+      return response.status(401).json({ error: "Email already exists." });
+    }
 
     const user = await User.create({
       id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     });
 
