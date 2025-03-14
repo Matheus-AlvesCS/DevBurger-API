d7185e8b73ce6943d8de9598a72fd91ee6412e68
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2025-03-14 19:55:10 -0300

    Tabela de produtos e criação de produtos

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { resolve } from "node:path";
+
 import router from "./routes.js";
 
 import "./database";
@@ -12,6 +14,10 @@ class App {
 
   middlewares() {
     this.app.use(express.json());
+    this.app.use(
+      "/product-file",
+      express.static(resolve(__dirname, "..", "uploads"))
+    );
   }
 
   routes() {
