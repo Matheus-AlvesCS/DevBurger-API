1de0882b02c9d57dc1cb821039a9289f8c1ad133
Matheus-AlvesCS <matheusalves.macs@gmail.com>
2025-03-19 19:31:04 -0300

    Update de produtos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -9,6 +9,7 @@ class ProductController {
       name: yup.string().required(),
       price: yup.number().required(),
       category_id: yup.number().required(),
+      offer: yup.boolean(),
     });
 
     try {
@@ -24,13 +25,14 @@ class ProductController {
     }
 
     const { filename: path } = request.file;
-    const { name, price, category_id } = request.body;
+    const { name, price, category_id, offer } = request.body;
 
     const product = await Product.create({
       name,
       price,
       category_id,
       path,
+      offer,
     });
 
     return response.status(201).json(product);
@@ -48,6 +50,59 @@ class ProductController {
     });
 
     return response.json(products);
+  }
+
+  async update(request, response) {
+    const productSchema = yup.object({
+      name: yup.string(),
+      price: yup.number(),
+      category_id: yup.number(),
+      offer: yup.boolean(),
+    });
+
+    try {
+      productSchema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId);
+
+    if (!isAdmin) {
+      return response.status(401).json({ message: "Not authorized." });
+    }
+
+    const { id } = request.params;
+
+    const findProduct = await Product.findByPk(id);
+
+    if (!findProduct) {
+      return response.status(400).json({ error: "Product not found." });
+    }
+
+    let path;
+    if (request.file) {
+      path = request.file.filename;
+    }
+
+    const { name, price, category_id, offer } = request.body;
+
+    await Product.update(
+      {
+        name,
+        price,
+        category_id,
+        path,
+        offer,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    return response.status(200);
   }
 }
 
